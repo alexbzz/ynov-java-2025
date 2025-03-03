ff4b58572873ac7d30f1f828246a864047c36254
Alexandre BOZZI <alexandre.bozzi@ynov.com>
2025-03-03 17:05:29 +0100

    exercices(2): boucles

---
+++ src/fr/ynov/java/easy/DoWhile.java
@@ -0,0 +1,13 @@
+package fr.ynov.java.easy;
+
+public class DoWhile {
+    public static void main(String[] args){
+        int i = -1;
+        do{
+            i++;
+            System.out.println(i);
+        }
+        while(i < 10);
+
+    }
+}
