26b302dbc9adfcbbbb01be92f154f49e5c8a1fa6
Alexandre BOZZI <alexandre.bozzi@ynov.com>
2025-03-10 16:38:21 +0100

    Array easy

---
+++ src/fr/ynov/java/easy/Array.java
@@ -0,0 +1,10 @@
+package fr.ynov.java.easy;
+
+    public class Array {
+        public static void main(String[] args) {
+            int[] numbers = {10, 20, 30, 40, 50};
+            for (int num : numbers) {
+                System.out.println(num);
+            }
+        }
+    }
